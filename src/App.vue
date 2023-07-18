3cb5dfa04d2a15ed5f10577d2515e8e254dff8e1
brunonp_ <brunonicolaspadin@gmail.com>
2023-07-18 17:08:06 -0300

    Add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import Button from "primevue/button"
+  import Menubar from 'primevue/menubar'
+	import { ref } from 'vue';
+  const items = ref([
+	{
+		label: 'Lovs',
+	},
+	{
+		label: 'Exercises',
+	},
+	{
+		label: 'Workout',
+	},
+	{
+		label: 'Training',
+	},
+	{
+		label: 'Marks',
+		items: [
+			{
+				label: 'Body',
+			},
+			{
+				label: '1RM',
+			},
+			{
+				label: 'Weight',
+			},
+			{
+				label: 'Goals',
+			}
+		]
+	},
+	{
+		label: 'Login',
+	},
+	{
+		label: 'Register',
+	},
+]);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  <Button label="Submit" />
+	<div class="card relative z-2">
+		<Menubar :model="items" />
+	</div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import PrimeVue from 'primevue/config'
-import "primevue/resources/themes/tailwind-light/theme.css";
+import "primevue/resources/themes/tailwind-light/theme.css"
+import 'primeicons/primeicons.css'
 
 const app = createApp(App)
 app.use(PrimeVue)
