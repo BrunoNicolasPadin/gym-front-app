6e2b623bd3d9cbde1c788e60e9336e4deb931c7f
brunonp_ <brunonicolaspadin@gmail.com>
2023-07-19 19:41:47 -0300

    Write route name correctly

--- src/components/BreadcrumbNav.vue
+++ src/components/BreadcrumbNav.vue
@@ -6,7 +6,7 @@
                     v-for="item in breadcrumbItems" :key="item.id"
                     :class="[ item.first ? 'inline-flex items-center' : '']"
                 >
-                    <router-link v-if="item.first" :to="item.routeName" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                    <router-link v-if="item.first" :to="{name: item.routeName}" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
                         <svg class="w-3 h-3 mr-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
                         </svg>
@@ -17,7 +17,7 @@
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
                         </svg>
                         <router-link 
-                            :to="item.routeName" 
+                            :to="{name: item.routeName}" 
                             class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">
                             {{ item.label }}
                         </router-link>
